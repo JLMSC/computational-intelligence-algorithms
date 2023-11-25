9671d722329496eadacb3890e52fc09d0ed02415
Joan Lucas <reecaptcha@gmail.com>
2023-11-25 12:54:03 -0300

    Rebase

---
+++ HC_/HillClimbing.py
@@ -0,0 +1,11 @@
+class HillClimbing:
+    """Hill Climbing (Subida de Enconsta)"""
+
+    def __init__(self) -> None:
+        # Ponto inicial: o valor de limite inferior do domínio de x.
+        # Gerar candidato a partir de x_best: |x_best - y| <= e; onde
+            # y é um possível candidato da vizinhaça
+            # e deve ser definido como um valor pequeno (0, 1)
+        # Fazer a identificação do menor valor de abertura que
+        # encontra a solução ótima.
+        pass
